d62bd71d49aea9ca1d9aea3fc56c5c2eab8ba819
John Flournoy <john.flournoy@gmail.com>
2020-10-08 18:05:16 -0700

    Create batch_simulations.bash

---
+++ batch_simulations.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -J sims_example
+#SBATCH --mem 16G
+#SBATCH -p ncf
+#SBATCH --cpus-per-task 1
+#SBATCH --nodes 1
+#SBATCH --ntasks 1
+#SBATCH --time 1-00:00
+#SBATCH -o %x_%A_%a.out
+#SBATCH --mail-type=ALL
+
+module load gcc/7.1.0-fasrc01
+module load R/3.5.1-fasrc01
+
+export R_LIBS_USER=/ncf/mclaughlin/users/jflournoy/R_3.5.1_GCC:$R_LIBS_USER
+
+runme=run_simulations.R
+
+srun -c $SLURM_CPUS_ON_NODE Rscript "${runme}"
